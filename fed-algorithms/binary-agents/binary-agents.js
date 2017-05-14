555a5d3973f28ddc85825f1e1f1e23ae579f3ecf
krantzinator <krantzrs8@gmail.com>
2017-05-13 22:55:28 -0400

    finished up binary agents

--- fed-algorithms/binary-agents/binary-agents.js
+++ fed-algorithms/binary-agents/binary-agents.js
@@ -1,7 +1,13 @@
 var Translator = function(){};
 
 Translator.prototype.binaryAgent = function(str) {
-  return str;
+  var counter = '';
+
+  str.split(' ').forEach(function(x){
+     counter += String.fromCharCode(parseInt(x, 2));
+  });
+
+  return counter;
 };
 
 module.exports = Translator;
