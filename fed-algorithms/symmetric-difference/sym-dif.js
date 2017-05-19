c647c1e310a91e19fa895252b3a37e079741336f
krantzinator <krantzrs8@gmail.com>
2017-05-18 22:55:04 -0400

    well this is crap

--- fed-algorithms/symmetric-difference/sym-dif.js
+++ fed-algorithms/symmetric-difference/sym-dif.js
@@ -2,18 +2,28 @@ var SymmetricDifference = function() {};
 
 SymmetricDifference.prototype.compute = function(args) {
   var argsArray = Array.prototype.slice.call(arguments);
-  args1 = argsArray[0];
-  args2 = argsArray[1];
-  
-  var newArray = args1.filter(function(x) {
-    return !args2.includes(x);
-  });
-  var secondNewArray = args2.filter(function(x) {
-    return !args1.includes(x);
-  });
+
+  // var newArray = args1.filter(function(x) {
+  //   return !args2.includes(x);
+  // });
+  // var secondNewArray = args2.filter(function(x) {
+  //   return !args1.includes(x);
+  // });
   // newArray.push(args2.filter(notPresent));
 
-  return newArray.concat(secondNewArray).sort();
+  return argsArray.reduce(function(acc, item){
+      console.log("1", acc, item);
+      return item.reduce(function(acc, item) {
+        console.log("2", acc, item);
+        if (!acc.includes(item)) {
+          acc.push(item);
+        } else {
+          // blahde
+        }
+        console.log(acc);
+        return acc;
+      }, acc);
+  }, []);
 };
 
 module.exports = SymmetricDifference;
